c1cbd0ef6680c4d533d364ba549747ec479a4e6d
Alona Ratsebarska <aleonorarace@gmail.com>
2022-08-16 00:49:33 +0300

    programm 006

---
+++ Lection_1_006/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Vvedite imia polzovatelia: ");
+string username = Console.ReadLine();
+
+if  (username.ToLower() == "maria") {
+       Console.WriteLine("Ura, eto zhe Maria!");
+}
+else {
+    Console.WriteLine("Privetstvuem Vas, " + username);
+}
