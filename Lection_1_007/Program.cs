125ac704de192057f2890dcd256cd702b4cdc0c4
Alona Ratsebarska <aleonorarace@gmail.com>
2022-08-16 01:05:45 +0300

    programm 007

---
+++ Lection_1_007/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 6;
+int b = 3;
+int c = 1;
+int d = 9;
+int e = 4;
+
+int max = a;
+ 
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
