b3841bec90b74bfa3b2b0eaf1c007c0a729983e8
Alona Ratsebarska <aleonorarace@gmail.com>
2022-08-21 18:59:36 +0300

    2.programm 3

---
+++ Lection_2_003/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 11, 21, 31, 4, 18, 15, 16, 17, 18 };
+int n = array.Length;
+
+int find = Convert.ToInt32(Console.ReadLine());
+//int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
