25b64ce8fed7720327ad595b4d625554800e7156
Alona Ratsebarska <aleonorarace@gmail.com>
2022-09-11 00:32:46 +0300

    Dvuchmernyje massivy

---
+++ Lection_4_001/Program.cs
@@ -0,0 +1,44 @@
+﻿/*string[,] table = new string[2, 5];
+table[1,2] = "slovo";
+
+for (int rows = 0; rows <2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+*/
+
+
+void PrintArray(int[,] matr)
+{
+    //int[,] matrix = new int[3, 4];
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for( int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows,columns] = new Random().Next(-20, 20);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+
