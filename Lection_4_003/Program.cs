819e00fc486c5578bc85e424caae4a04c07d5546
Alona Ratsebarska <aleonorarace@gmail.com>
2022-09-11 03:47:00 +0300

    Rekursija: Faktorial i chisla Fibonacci

---
+++ Lection_4_003/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+double Factorial(int n)
+{
+    if (n == 1 || n == 0) return 1;
+    else return n * Factorial(n-1);
+}
+Console.WriteLine(Factorial(5));
+*/
+
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n -1) + Fibonacci(n-2);
+
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
